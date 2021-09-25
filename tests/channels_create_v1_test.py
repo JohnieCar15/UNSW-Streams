47379467360e5119d15bb1d7d2fd2d394cb1ba36
Lawrence Chau <z5360392@ad.unsw.edu.au>
2021-09-25 17:03:18 +1000

    Added fixtures

--- tests/channels_create_v1_test.py
+++ tests/channels_create_v1_test.py
@@ -6,27 +6,24 @@ from src.channels import channels_create_v1
 from src.error import InputError
 from src.error import AccessError
 
-# change later for implementation
-def test_valid_id_valid_name_public():
+@pytest.fixture
+def clear_and_register():
     clear_v1()
     id = auth_register_v1("z0000000@gmail.com" , "aaaaaa", "firstname", "lastname" )
+
+# change later for implementation
+def test_valid_id_valid_name_public(clear_and_register):
     assert channels_create_v1(id, 'name', True) == 1 
 
 # change later for implementation 
-def test_valid_id_valid_name_private():
-    clear_v1()
-    id = auth_register_v1("z0000000@gmail.com" , "aaaaaa", "firstname", "lastname" )
+def test_valid_id_valid_name_private(clear_and_register):
     assert channels_create_v1(id, 'name' , False) == 1 
     
-def test_valid_id_invalid_name():
-    clear_v1()
-    id = auth_register_v1("z0000000@gmail.com" , "aaaaaa", "firstname", "lastname" )
+def test_valid_id_invalid_name(clear_and_register):
     with pytest.raises(InputError):
         assert channels_create_v1(id,'' , True) 
 
-def test_valid_id_invalid_name_2():
-    clear_v1()
-    id = auth_register_v1("z0000000@gmail.com" , "aaaaaa", "firstname", "lastname" )
+def test_valid_id_invalid_name_2(clear_and_register):
     with pytest.raises(InputError):
         assert channels_create_v1(id,'aaaaaaaaaaaaaaaaaaaaa' , True) 
 
