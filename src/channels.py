e69d27adee42c3237b9fc258898f37958793f33f
Lawrence Chau <z5360392@ad.unsw.edu.au>
2021-09-28 02:51:30 +1000

    Implemented channels_create_v1 passes all tests

--- src/channels.py
+++ src/channels.py
@@ -23,27 +23,29 @@ def channels_listall_v1(auth_user_id):
     }
 
 def channels_create_v1(auth_user_id, name, is_public):
+    
     store = data_store.get()
     
-    is_valid = 0
-    # check if auth_user_id is valid
+    check = 0
+
+    # check if user_id valid 
     for user in store['users']:
         if user[0] == auth_user_id:
-            is_valid = 1
+            check = 1
 
-    if is_valid == 0:
-       raise AccessError("Invalid user id")
-    
+    if check == 0:
+        raise AccessError
+
     # check if name is valid
     if (len(name) < 1 or len(name) > 20):
         raise InputError("Invalid name length")
-
-
+    
+    
     # channel id will be len of existing list + 1 
     new_id = len(store['channels']) + 1
     # store a tuple containing the following 
     store['channels'].append((new_id, name, auth_user_id, is_public))
-    data_store.set[store]
+    
     return {
         'channel_id': new_id,
     }
