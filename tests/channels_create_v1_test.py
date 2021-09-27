feea16ba15d1815d6cc7f09de97c4037672c2c26
Lawrence Chau <z5360392@ad.unsw.edu.au>
2021-09-28 02:51:30 +1000

    Implemented channels_create_v1 passes all tests

--- tests/channels_create_v1_test.py
+++ tests/channels_create_v1_test.py
@@ -6,22 +6,28 @@ from src.channels import channels_create_v1
 from src.error import InputError
 from src.error import AccessError
 
+
 @pytest.fixture
 def clear_and_register():
     clear_v1()
     id = auth_register_v1("z0000000@gmail.com" , "aaaaaa", "firstname", "lastname" )
+    return id['auth_user_id']
 
-# change later for implementation
+
 def test_valid_id_valid_name_public(clear_and_register):
-    assert channels_create_v1(id, 'name', True) == {'channel_id': 1}
-    
+    id_return = clear_and_register
+    assert channels_create_v1(id_return, 'name', True) == {'channel_id': 1}
+
+
 def test_valid_id_invalid_name(clear_and_register):
+    id_return = clear_and_register
     with pytest.raises(InputError):
-        assert channels_create_v1(id,'' , True) 
+        assert channels_create_v1(id_return,'' , True) 
 
 def test_valid_id_invalid_name_2(clear_and_register):
+    id_return = clear_and_register
     with pytest.raises(InputError):
-        assert channels_create_v1(id,'aaaaaaaaaaaaaaaaaaaaa' , True) 
+        assert channels_create_v1(id_return,'aaaaaaaaaaaaaaaaaaaaa' , True) 
 
 def test_invalid_id_invalid_name():
     clear_v1()
