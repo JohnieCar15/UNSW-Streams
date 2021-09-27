94e24958705e42c7ec6582ece70854231d34e81b
Dylan Beretov <z5359599@ad.unsw.edu.au>
2021-09-28 00:36:44 +1000

    Implemented auth_login_v1 function and fixed tests for auth_login_v1

--- src/auth.py
+++ src/auth.py
@@ -2,9 +2,18 @@ from src.data_store import data_store
 from src.error import InputError
 
 def auth_login_v1(email, password):
-    return {
-        'auth_user_id': 1,
-    }
+    store = data_store.get()
+
+    for user in store['users']:
+        if user[1] == email:
+            if user[2] == password:
+                return {
+                    'auth_user_id': auth_user_id,
+                }
+            else:
+                raise InputError("Incorrect password")
+    
+    raise InputError("Email is not registered")
 
 def auth_register_v1(email, password, name_first, name_last):
     return {
--- tests/auth_login_v1_test.py
+++ tests/auth_login_v1_test.py
@@ -5,20 +5,24 @@ from src.auth import auth_register_v1
 from src.other import clear_v1
 from src.error import InputError
 
-def test_auth_login_v1():
-    clear_v1()
-    auth_register_v1("valid@gmail.com", "password", "First", "Last")
-
-    assert auth_login_v1("valid@gmail.com", "password") == 0
-
-def test_unregistered_email():
+@pytest.fixture
+def clear():
     clear_v1()
 
+def test_auth_login_v1(clear):
+    register_return = auth_register_v1("valid@gmail.com", "password", "First", "Last")
+    register_auth_user_id = register_return['auth_user_id']
+
+    login_return = auth_login_v1("valid@gmail.com", "password")
+    login_auth_user_id = login_return['auth_user_id']
+
+    assert register_auth_user_id == login_auth_user_id
+
+def test_unregistered_email(clear):
     with pytest.raises(InputError):
         assert auth_login_v1("unregistered@gmail.com", "password")
     
-def test_incorrect_password():
-    clear_v1()
+def test_incorrect_password(clear):
     auth_register_v1("valid@gmail.com", "password", "First", "Last")
 
     with pytest.raises(InputError):
