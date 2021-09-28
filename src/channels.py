f058c9836af3076ed84161fa60c28293de91e34e
Haokai <haokai.yin@student.unsw.edu.au>
2021-09-28 14:34:18 +0800

    Implemented channels_list_v1 function

--- src/channels.py
+++ src/channels.py
@@ -3,14 +3,21 @@ from src.error import InputError
 from src.error import AccessError
 
 def channels_list_v1(auth_user_id):
-    return {
-        'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
-        ],
-    }
+    store = data_store.get()
+
+    # check if user_id valid
+    if auth_user_id not in [user[0] for user in store['users']]:
+        raise AccessError("Invalid user_id")
+    list_of_channel = []
+    
+    for channel in store['channels']:
+        channel_details = {}
+        if auth_user_id in channel['members']:
+            channel_details['channel_id'] = channel['id']
+            channel_details['name'] = channel['name']
+            list_of_channel.append(channel_details)
+
+    return { 'channels': list_of_channel }
 
 def channels_listall_v1(auth_user_id):
     return {
