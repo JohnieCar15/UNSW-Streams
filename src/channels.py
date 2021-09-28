db3a3bb96f63f323211414734786cec9d9ba9284
Lawrence Chau <z5360392@ad.unsw.edu.au>
2021-09-28 14:49:12 +1000

    Added a list for members inside [channels]

--- src/channels.py
+++ src/channels.py
@@ -37,7 +37,7 @@ def channels_create_v1(auth_user_id, name, is_public):
     # channel id will be len of existing list + 1 
     new_id = len(store['channels']) + 1
     # store a tuple containing the following 
-    store['channels'].append((new_id, name, auth_user_id, is_public))
+    store['channels'].append([new_id, name, auth_user_id, is_public,[auth_user_id]])
     
     return {
         'channel_id': new_id,
