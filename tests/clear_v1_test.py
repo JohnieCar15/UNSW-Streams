d51ade743a023152bc279711c420a1db4ba6c797
Johnie Carino <z5361576@ad.unsw.edu.au>
2021-09-28 15:52:43 +1000

    Uncommented first two tests

--- tests/clear_v1_test.py
+++ tests/clear_v1_test.py
@@ -6,24 +6,13 @@ from src.channels import channels_create_v1
 from src.channel import channel_details_v1, channel_join_v1, channel_messages_v1
 from src.error import InputError
 
-'''
-@pytest.fixture
-def user_id():
-    userid = auth_register_v1("valid@gmail.com", "password", "First", "Last")
-    return userid
-
-@pytest.fixture
-def channel_id():
-    channelid = channels_create_v1(user_id, "channel", True)
-    return channelid
-
 def test_register_login():
     auth_register_v1("valid@gmail.com", "password", "First", "Last")
     clear_v1()
     
     with pytest.raises(InputError):
         assert(auth_login_v1("valid@gmail.com", "password"))
-'''
+
 
 def test_register_twice():
     auth_register_v1("valid@gmail.com", "password", "First", "Last")
