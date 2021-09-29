d1b70ffd4be722dd0f6217f1c8b9b2e94d3b5b36
Johnie Carino <z5361576@ad.unsw.edu.au>
2021-09-29 15:14:10 +1000

    removed assert statements from test

--- tests/channel_messages_v1_test.py
+++ tests/channel_messages_v1_test.py
@@ -25,14 +25,14 @@ def test_invalid_start():
     valid_channel_id = channels_create_v1(valid_user_id, "channel", True)
 
     with pytest.raises(InputError):
-        assert(channel_messages_v1(valid_user_id, valid_channel_id, 10))
+        channel_messages_v1(valid_user_id, valid_channel_id, 10)
 
 def test_invalid_channel_id():
     clear_v1()
     valid_user_id = auth_register_v1("valid@gmail.com", "password", "First", "Last")
 
     with pytest.raises(InputError):
-        assert(channel_messages_v1(valid_user_id, 10, 0))
+        channel_messages_v1(valid_user_id, 10, 0)
 
 def test_invalid_user_id():
     clear_v1()
@@ -40,12 +40,12 @@ def test_invalid_user_id():
     valid_channel_id = channels_create_v1(valid_user_id, "channel", True)
 
     with pytest.raises(AccessError):
-        assert(channel_messages_v1(valid_user_id + 1, valid_channel_id, 0))
+        channel_messages_v1(valid_user_id + 1, valid_channel_id, 0)
 
 def test_invalid_channel_id_invalid_user_id():
     clear_v1()
     with pytest.raises(AccessError):
-        assert(channel_messages_v1(10, 10, 0))
+        channel_messages_v1(10, 10, 0)
 
 def test_invalid_start_invalid_id():
     clear_v1()
@@ -53,14 +53,14 @@ def test_invalid_start_invalid_id():
     valid_channel_id = channels_create_v1(valid_user_id, "channel", True)
 
     with pytest.raises(AccessError):
-        assert(channel_messages_v1(valid_user_id + 1, valid_channel_id, 10))
+        channel_messages_v1(valid_user_id + 1, valid_channel_id, 10)
 
 def test_invalid_start_invalid_channel():
     clear_v1()
     valid_user_id = auth_register_v1("valid@gmail.com", "password", "First", "Last")
 
     with pytest.raises(InputError):
-        assert(channel_messages_v1(valid_user_id, 10, 10))
+        channel_messages_v1(valid_user_id, 10, 10)
 
 
 
