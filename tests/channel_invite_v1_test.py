f8b5d939c0d605e4d30f031643b3a186dde92785
Krishen T <z5362990@unsw.edu.au>
2021-09-30 20:28:50 +1000

    Tidied and fixed issues with channel_invite tests

--- tests/channel_invite_v1_test.py
+++ tests/channel_invite_v1_test.py
@@ -9,29 +9,29 @@ from src.error import InputError, AccessError
 
 def test_public_channel_invite():
     clear_v1()
-    valid_auth_id = auth_register_v1('auth@gmail.com', 'password', 'First', 'Last')['auth_user_id']
-    valid_invitee_id = auth_register_v1('user@gmail.com', 'password', 'First', 'Last')['auth_user_id']
-    valid_channel_id = channels_create_v1(valid_auth_id, 'Channel', True)['channel_id']
-    channel_join_v1(valid_auth_id, valid_channel_id)
-    channel_invite_v1(valid_auth_id, valid_channel_id, valid_invitee_id)
+    auth_id = auth_register_v1('auth@gmail.com', 'password', 'First', 'Last')['auth_user_id']
+    invitee_id = auth_register_v1('user@gmail.com', 'password', 'First', 'Last')['auth_user_id']
+    public_channel_id = channels_create_v1(auth_id, 'Channel', True)['channel_id']
+    
+    channel_invite_v1(auth_id, public_channel_id, invitee_id)
 
-    channel_members = channel_details_v1(valid_auth_id, valid_channel_id)['all_members']
+    channel_members = channel_details_v1(auth_id, public_channel_id)['all_members']
     channel_members_ids = [user['u_id'] for user in channel_members]
 
-    assert valid_invitee_id in channel_members_ids
+    assert invitee_id in channel_members_ids
 
 def test_private_channel_invite():
     clear_v1()
-    valid_auth_id = auth_register_v1('auth@gmail.com', 'password', 'First', 'Last')['auth_user_id']
-    valid_invitee_id = auth_register_v1('user@gmail.com', 'password', 'First', 'Last')['auth_user_id']
-    valid_channel_id = channels_create_v1(valid_auth_id, 'Channel', False)['channel_id']
-    channel_join_v1(valid_auth_id, valid_channel_id)
-    channel_invite_v1(valid_auth_id, valid_channel_id, valid_invitee_id)
+    auth_id = auth_register_v1('auth@gmail.com', 'password', 'First', 'Last')['auth_user_id']
+    invitee_id = auth_register_v1('user@gmail.com', 'password', 'First', 'Last')['auth_user_id']
+    private_channel_id = channels_create_v1(auth_id, 'Channel', False)['channel_id']
+    
+    channel_invite_v1(auth_id, private_channel_id, invitee_id)
 
-    channel_members = channel_details_v1(valid_auth_id, valid_channel_id)['all_members']
+    channel_members = channel_details_v1(auth_id, private_channel_id)['all_members']
     channel_members_ids = [user['u_id'] for user in channel_members]
 
-    assert valid_invitee_id in channel_members_ids
+    assert invitee_id in channel_members_ids
 
 
 def test_invalid_auth_id():
@@ -39,7 +39,6 @@ def test_invalid_auth_id():
     valid_auth_id = auth_register_v1('auth@gmail.com', 'password', 'First', 'Last')['auth_user_id']
     valid_invitee_id = auth_register_v1('user@gmail.com', 'password', 'First', 'Last')['auth_user_id']
     valid_channel_id = channels_create_v1(valid_auth_id, 'Channel', False)['channel_id']
-    channel_join_v1(valid_auth_id, valid_channel_id)
 
     invalid_auth_id = valid_auth_id + 1
     if invalid_auth_id == valid_invitee_id:
@@ -53,7 +52,6 @@ def test_invalid_invitee_id():
     valid_auth_id = auth_register_v1('auth@gmail.com', 'password', 'First', 'Last')['auth_user_id']
     valid_invitee_id = auth_register_v1('user@gmail.com', 'password', 'First', 'Last')['auth_user_id']
     valid_channel_id = channels_create_v1(valid_auth_id, 'Channel', False)['channel_id']
-    channel_join_v1(valid_auth_id, valid_channel_id)
 
     invalid_invitee_id = valid_invitee_id + 1
     if invalid_invitee_id == valid_auth_id:
@@ -67,7 +65,6 @@ def test_invalid_channel_id():
     valid_auth_id = auth_register_v1('valid@gmail.com', 'password', 'First', 'Last')['auth_user_id']
     valid_invitee_id = auth_register_v1('user@gmail.com', 'password', 'First', 'Last')['auth_user_id']
     valid_channel_id = channels_create_v1(valid_auth_id, 'Channel', True)['channel_id']
-    channel_join_v1(valid_auth_id, valid_channel_id)
 
     invalid_channel_id = valid_channel_id + 1
     with pytest.raises(InputError):
@@ -78,7 +75,6 @@ def test_all_ids_invalid():
     valid_auth_id = auth_register_v1('auth@gmail.com', 'password', 'First', 'Last')['auth_user_id']
     valid_invitee_id = auth_register_v1('user@gmail.com', 'password', 'First', 'Last')['auth_user_id']
     valid_channel_id = channels_create_v1(valid_auth_id, 'Channel', False)['channel_id']
-    channel_join_v1(valid_auth_id, valid_channel_id)
 
     invalid_channel_id = valid_channel_id + 1
 
@@ -99,7 +95,6 @@ def test_duplicate_invite():
     auth_id = auth_register_v1('auth@gmail.com', 'password', 'First', 'Last')['auth_user_id']
     invitee_id = auth_register_v1('user@gmail.com', 'password', 'First', 'Last')['auth_user_id']
     channel_id = channels_create_v1(auth_id, 'Channel', False)['channel_id']
-    channel_join_v1(auth_id, channel_id)
 
     channel_invite_v1(auth_id, channel_id, invitee_id)
     with pytest.raises(InputError):
@@ -108,9 +103,10 @@ def test_duplicate_invite():
 def test_inviter_not_in_channel():
     clear_v1()
     auth_id = auth_register_v1('auth@gmail.com', 'password', 'First', 'Last')['auth_user_id']
-    invitee_id = auth_register_v1('user@gmail.com', 'password', 'First', 'Last')['auth_user_id']
+    inviter_id = auth_register_v1('user1@gmail.com', 'password', 'First', 'Last')['auth_user_id']
+    invitee_id = auth_register_v1('user2@gmail.com', 'password', 'First', 'Last')['auth_user_id']
     channel_id = channels_create_v1(auth_id, 'Channel', False)['channel_id']
 
     with pytest.raises(AccessError):
-        channel_invite_v1(auth_id, channel_id, invitee_id)
+        channel_invite_v1(inviter_id, channel_id, invitee_id)
 
