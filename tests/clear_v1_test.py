cadc20e11c62cdb2c57764c8db748f5f5046e846
Johnie Carino <z5361576@ad.unsw.edu.au>
2021-10-02 13:29:46 +1000

    Uncommented tests

--- tests/clear_v1_test.py
+++ tests/clear_v1_test.py
@@ -4,9 +4,10 @@ from src.other import clear_v1
 from src.auth import auth_register_v1, auth_login_v1
 from src.channels import channels_create_v1
 from src.channel import channel_details_v1, channel_join_v1, channel_messages_v1
-from src.error import InputError
+from src.error import InputError, AccessError
 
 def test_register_login():
+    clear_v1()
     auth_register_v1("valid@gmail.com", "password", "First", "Last")
     clear_v1()
     
@@ -15,33 +16,36 @@ def test_register_login():
 
 
 def test_register_twice():
+    clear_v1()
     auth_register_v1("valid@gmail.com", "password", "First", "Last")
     clear_v1()
     auth_register_v1("valid@gmail.com", "password", "First", "Last")
 
-'''
+
 def test_channel_details():
-    user_id = auth_register_v1("valid@gmail.com", "password", "First", "Last")
-    channel_id = channels_create_v1(user_id, "channel", True)
+    clear_v1()
+    user_id = auth_register_v1("valid@gmail.com", "password", "First", "Last")['auth_user_id']
+    channel_id = channels_create_v1(user_id, "channel", True)['channel_id']
 
     clear_v1()
     with pytest.raises(InputError):
         channel_details_v1(user_id, channel_id)
 
 def test_channel_join():
-    user_id = auth_register_v1("valid@gmail.com", "password", "First", "Last")
-    channel_id = channels_create_v1(user_id, "channel", True)
+    clear_v1()
+    user_id = auth_register_v1("valid@gmail.com", "password", "First", "Last")['auth_user_id']
+    channel_id = channels_create_v1(user_id, "channel", True)['channel_id']
 
     clear_v1()
-    with pytest.raises(InputError):
+    with pytest.raises(AccessError):
         channel_join_v1(user_id, channel_id)
 
 def test_channel_message():
-    user_id = auth_register_v1("valid@gmail.com", "password", "First", "Last")
-    channel_id = channels_create_v1(user_id, "channel", True)
+    clear_v1()
+    user_id = auth_register_v1("valid@gmail.com", "password", "First", "Last")['auth_user_id']
+    channel_id = channels_create_v1(user_id, "channel", True)['channel_id']
 
     clear_v1()
     with pytest.raises(InputError):
         channel_messages_v1(user_id, channel_id, 0)
 
-'''
