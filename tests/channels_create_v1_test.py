055bd7751dafa5c2b9f55a7b950c70ea862df91b
Lawrence Chau <z5360392@ad.unsw.edu.au>
2021-10-02 14:12:21 +1000

    Updated tests for private and public

--- tests/channels_create_v1_test.py
+++ tests/channels_create_v1_test.py
@@ -22,31 +22,58 @@ def test_valid_id_valid_name_private(clear_and_register):
     id_return = clear_and_register
     assert channels_create_v1(id_return, 'name', False) == {'channel_id': 1}
 
-def test_valid_id_invalid_name(clear_and_register):
+def test_valid_id_invalid_name_public(clear_and_register):
     id_return = clear_and_register
     with pytest.raises(InputError):
         channels_create_v1(id_return,'' , True) 
 
-def test_valid_id_invalid_name_2(clear_and_register):
+def test_valid_id_invalid_name_private(clear_and_register):
+    id_return = clear_and_register
+    with pytest.raises(InputError):
+        channels_create_v1(id_return,'' , False)
+
+def test_valid_id_invalid_name_2_public(clear_and_register):
     id_return = clear_and_register
     with pytest.raises(InputError):
         channels_create_v1(id_return,'aaaaaaaaaaaaaaaaaaaaa' , True) 
 
-def test_invalid_id_invalid_name():
+def test_valid_id_invalid_name_2_private(clear_and_register):
+    id_return = clear_and_register
+    with pytest.raises(InputError):
+        channels_create_v1(id_return,'aaaaaaaaaaaaaaaaaaaaa' , False) 
+
+def test_invalid_id_invalid_name_public():
     clear_v1()
     id = -1
     with pytest.raises(AccessError):
         channels_create_v1(id, "", True)
 
-def test_invalid_id_invalid_name_2():
+def test_invalid_id_invalid_name_private():
+    clear_v1()
+    id = -1
+    with pytest.raises(AccessError):
+        channels_create_v1(id, "", False)
+
+def test_invalid_id_invalid_name_2_public():
     clear_v1()
     id = -1
     with pytest.raises(AccessError):
         channels_create_v1(id, "aaaaaaaaaaaaaaaaaaaaa", True)
 
-def test_invalid_id_valid_name():
+def test_invalid_id_invalid_name_2_private():
+    clear_v1()
+    id = -1
+    with pytest.raises(AccessError):
+        channels_create_v1(id, "aaaaaaaaaaaaaaaaaaaaa", False)
+
+def test_invalid_id_valid_name_public():
     clear_v1()
     id = -1
     with pytest.raises(AccessError):
         channels_create_v1(id, "name", True)
 
+def test_invalid_id_valid_name_private():
+    clear_v1()
+    id = -1
+    with pytest.raises(AccessError):
+        channels_create_v1(id, "name", False)
