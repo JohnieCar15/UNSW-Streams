eb03fc0c2b9d5b1c568b7f069bf4678b7373ccbe
Dylan Beretov <z5359599@ad.unsw.edu.au>
2021-10-02 16:08:54 +1000

    Fixed handle functionality, added comments

--- tests/auth_login_v1_test.py
+++ tests/auth_login_v1_test.py
@@ -1,7 +1,6 @@
 import pytest
 
-from src.auth import auth_login_v1
-from src.auth import auth_register_v1
+from src.auth import auth_login_v1, auth_register_v1
 from src.other import clear_v1
 from src.error import InputError
 
@@ -9,6 +8,7 @@ from src.error import InputError
 def clear():
     clear_v1()
 
+# Test if function works when given correct input
 def test_auth_login_v1(clear):
     register_return = auth_register_v1("valid@gmail.com", "password", "First", "Last")
     register_auth_user_id = register_return['auth_user_id']
@@ -18,10 +18,12 @@ def test_auth_login_v1(clear):
 
     assert register_auth_user_id == login_auth_user_id
 
+# Test if function is given an unregistered email
 def test_unregistered_email(clear):
     with pytest.raises(InputError):
         auth_login_v1("unregistered@gmail.com", "password")
-    
+
+# Test if function is given an incorrect password  
 def test_incorrect_password(clear):
     auth_register_v1("valid@gmail.com", "password", "First", "Last")
 
