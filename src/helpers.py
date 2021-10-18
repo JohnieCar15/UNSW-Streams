5acaa335a2385832831817685c06cff6b7f8bbfd
Krishen T <z5362990@unsw.edu.au>
2021-10-18 14:22:47 +1100

    Tidied helpers.py function

--- src/helpers.py
+++ src/helpers.py
@@ -1,8 +1,6 @@
 import jwt
 from src.error import AccessError
 from src.data_store import data_store
-#from error import AccessError
-#from data_store import data_store
 
 SESSION_TRACKER = 0
 SECRET = 'COMP1531'
@@ -55,14 +53,16 @@ def validate_token(encoded_jwt):
     return decoded_jwt
     #return decoded_jwt['user_id']
 
-
+# Used to replace list comprehensions when filtering the data store
 def filter_data_store(list, key=None, value=None):
     store = data_store.get()
+
     if value is not None and key is not None:
         filtered_list = [item for item in store[list] if item[key] == value]
         if len(filtered_list) == 0:
             return None
         return filtered_list[0]
+        
     elif key is not None:
         return [item[key] for item in store[list]]
 
