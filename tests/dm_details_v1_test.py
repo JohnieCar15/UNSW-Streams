f576756d1f9d634e45f6840a331b027d5272df0d
Lawrence Chau <z5360392@ad.unsw.edu.au>
2021-10-21 14:14:30 +1100

    Changed params to json

--- tests/dm_details_v1_test.py
+++ tests/dm_details_v1_test.py
@@ -8,14 +8,14 @@ from src.error import InputError, AccessError
 @pytest.fixture
 def clear_and_register():
     requests.delete(config.url + 'clear/v2')
-    register = requests.post(config.url + 'auth/register/v2', params={'email': "yes@yes.com", 'password': "aaaaaa", 'name_first': "firstname", "name_last": "lastname"})
+    register = requests.post(config.url + 'auth/register/v2', json={'email': "yes@yes.com", 'password': "aaaaaa", 'name_first': "firstname", "name_last": "lastname"})
     register_data = register.json()
     token = register_data['token']
-    register_2 = requests.post(config.url + 'auth/register/v2', params={'email': "yes2@yes.com", 'password': "aaaaaa", 'name_first': "name", "name_last": "name"})
+    register_2 = requests.post(config.url + 'auth/register/v2', json={'email': "yes2@yes.com", 'password': "aaaaaa", 'name_first': "name", "name_last": "name"})
     register_2_data = register.json()
     u_id = register_2_data['auth_user_id']
     # create a dm and get its dm id
-    dm_create = requests.post(config.url + 'dm/create/v1', params={'token': token, 'u_ids': [u_id]})
+    dm_create = requests.post(config.url + 'dm/create/v1', json={'token': token, 'u_ids': [u_id]})
     dm_create_data = dm_create.json()
 
     return {'token': token, 'dm_id':dm_create_data['dm_id'], 'u_id': register_data['auth_user_id'], 'u_id_2': register_2_data['auth_user_id']}
@@ -27,7 +27,7 @@ def valid_channel_authorised_member(clear_and_register):
     id_num = clear_and_register['u_id']
     id_num_2 = clear_and_register['u_id_2']
 
-    dm_details = requests.post(config.url + 'dm/details/v1', params={'token': token, 'dm_id': dm_id})
+    dm_details = requests.get(config.url + 'dm/details/v1', params={'token': token, 'dm_id': dm_id})
     dm_details_data = dm_details.json()
     assert dm_details_data == {
         'name': 'firstnamelastname, namename',
@@ -54,7 +54,7 @@ def valid_channel_authorised_member(clear_and_register):
 def valid_channel_unauthorised_member(clear_and_register):
     dm_id = clear_and_register['dm_id']
     
-    register = requests.post(config.url + 'auth/register/v2', params={'email': "yes2@yes.com", 'password': "aaaaaa", 'name_first': "name", "name_last": "name"})
+    register = requests.post(config.url + 'auth/register/v2', json={'email': "yes2@yes.com", 'password': "aaaaaa", 'name_first': "name", "name_last": "name"})
     register_data = register.json()
     token_2 = register_data["token"]
 
@@ -70,7 +70,7 @@ def valid_channel_invalid_token(clear_and_register):
 # an invalid dm valid token
 def invalid_channel_invalid_token():
     requests.delete(config.url + 'clear/v2')
-    register = requests.post(config.url + 'auth/register/v2', params={'email': "yes@yes.com", 'password': "aaaaaa", 'name_first': "firstname", "name_last": "lastname"})
+    register = requests.post(config.url + 'auth/register/v2', json={'email': "yes@yes.com", 'password': "aaaaaa", 'name_first': "firstname", "name_last": "lastname"})
     register_data = register.json()
     token = register_data['token']
     dm_details = requests.get(config.url + 'dm/details/v1', params={'token': token, 'dm_id': 1})
