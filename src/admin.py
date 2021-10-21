e10245581497c6adc594e4f1180e0d94b04575ba
Krishen T <z5362990@unsw.edu.au>
2021-10-21 18:36:51 +1100

    Fixed style for admin_user_remove and added additional helper function

--- src/admin.py
+++ src/admin.py
@@ -5,58 +5,71 @@ from src.helpers import validate_token, filter_data_store
 
 # Helper Function
 def is_global_owner(u_id):
-    user_dict = filter_data_store(list='users', key='id',value=u_id)
+    user_dict = filter_data_store(store='users', key='id',value=u_id)
     return user_dict['permission_id'] == 1
 
 # Helper Function
 def is_last_global_owner(u_id):
-    global_owner_list = filter_data_store(list='users', key='permission_id', value=1)
+    global_owner_list = filter_data_store(store='users', key='permission_id', value=1)
     return len(global_owner_list) == 1 and global_owner_list[0]['id'] == u_id
 
-
-def admin_user_remove_v1(token, u_id):
-    store = data_store.get()
-    auth_user_dict = validate_token(token)
-
-    if not is_global_owner(auth_user_dict['user_id']):
-        raise AccessError(description='The authorised user is not a global owner')
-
-    removed_user_dict = filter_data_store(list='users', key='id', value=u_id)
-    if removed_user_dict is None:
-        raise InputError(description='u_id does not refer to a valid user')
-
-    if is_last_global_owner(u_id):
-        raise InputError(description='u_id refers the only global owner')
-
-
-
-    # remove from dm and channel (could replace lines with channel/list and dm/list)
-    filtered_channel_list = [channel for channel in store['channel'] if u_id in channel['members']]
-    filtered_dm_list = [dm for dm in store['dm'] if u_id in dm['members']]
-
-    for channel_dict in filtered_channel_list:
+# Helper Function
+def remove_user_from_all_channels(channel_list, u_id):
+    for channel_dict in channel_list:
         # Replacing contents of messages with 'Removed user'
         for message_dict in channel_dict['messages']:
             if message_dict['u_id'] == u_id:
                 message_dict['message'] = 'Removed user'
         
-        # Removing user from channel_dict
+        # Removing user from list of channel members
         channel_dict['members'].remove(u_id)
-        # Need to change this to deal with iteration 2 multiple owners
+        # Removing user from list of channel owners if applicable
         if u_id in channel_dict['owner']:
             channel_dict['owner'].remove(u_id)
 
-    for dm_dict in filtered_dm_list:
-        # Replacing contents of messages with 'Removed user'
-        for message_dict in dm_dict['messages']:
-            if message_dict['u_id'] == u_id:
-                message_dict['message'] = 'Removed user'
 
-        # Removing user from dm_dict
-        dm_dict['members'].remove(u_id)
-        # Not sure about this code and what happens to owners after profile is deleted
-        '''if u_id in dm_dict['owner']:
-            dm_dict['owner'].remove(u_id)'''
+def admin_user_remove_v1(token, u_id):
+    '''
+    admin_user_remove_v1: Given a user by their u_id, remove them from the Streams.
+    Arguments:
+        token (str)         - Token string used to authorise and authenticate the user
+        u_id (int)          - User id of the user that the admin wishes to remove
+
+    Exceptions:
+        InputError  - Occurs when u_id does not refer to a valid user
+                    - Occurs when u_id refers to a user who is the only global owner
+        AccessError - Occurs when the authorised user is not a global owner
+                    - Occurs when the token passed in is not a valid token
+                    
+    Return Value:
+        Returns {} on successful token and u_id
+    '''
+    store = data_store.get()
+    # Checking that the token is valid and returning the decoded token
+    auth_user_dict = validate_token(token)
+
+    # Checking the auth user is global owner
+    if not is_global_owner(auth_user_dict['user_id']):
+        raise AccessError(description='The authorised user is not a global owner')
+
+    # Checking the u_id entered is valid
+    removed_user_dict = filter_data_store(store='users', key='id', value=u_id)
+    if removed_user_dict is None:
+        raise InputError(description='u_id does not refer to a valid user')
+
+    # Checking if the u_id refers to the last global owner
+    if is_last_global_owner(u_id):
+        raise InputError(description='u_id refers the only global owner')
+
+    # remove from dm and channel (could replace lines with channel/list and dm/list)
+    #filtered_channel_list = [channel for channel in store['channel'] if u_id in channel['members']]
+    #filtered_dm_list = [dm for dm in store['dm'] if u_id in dm['members']]
+    filtered_channel_list = filter_data_store(store='channels', key='members', value=u_id)
+    remove_user_from_all_channels(filtered_channel_list, u_id)
+
+    filtered_dm_list = filter_data_store(store='dms', key='members', value=u_id)
+    remove_user_from_all_channels(filtered_dm_list, u_id)
+
 
     # Removing users and setting relevant parameters
     removed_user_dict['is_removed'] = True
@@ -69,7 +82,5 @@ def admin_user_remove_v1(token, u_id):
     
     data_store.set(store)
     return {}
-
-
 
     
--- src/helpers.py
+++ src/helpers.py
@@ -3,11 +3,7 @@ from src.error import AccessError
 from src.data_store import data_store
 
 SESSION_TRACKER = 0
-<<<<<<< HEAD
-SECRET = 'COMP1531'
-=======
 SECRET = 'JOjQqnzcMKrLVsTVLNc2hzA4iWkqqcQB'
->>>>>>> master
 
 def generate_new_session_id():
     global SESSION_TRACKER
@@ -25,7 +21,7 @@ def decode_jwt(encoded_jwt):
     
 
 def validate_token(encoded_jwt):
-    user_id_list = filter_data_store(list='users', key='id')
+    user_id_list = filter_data_store(store='users', key='id')
     try:
         decoded_jwt = decode_jwt(encoded_jwt)
     except Exception:
@@ -35,7 +31,7 @@ def validate_token(encoded_jwt):
     if decoded_jwt is None or decoded_jwt['user_id'] not in user_id_list:
         raise AccessError(description='Invalid Token')
     else:
-        session_id_list = filter_data_store(list='users', key='id', value=decoded_jwt['user_id'])
+        session_id_list = filter_data_store(store='users', key='id', value=decoded_jwt['user_id'])
         if decoded_jwt['session_id'] not in session_id_list[0]['session_list']:
             raise AccessError(description='Invalid Token')
     
@@ -43,16 +39,16 @@ def validate_token(encoded_jwt):
     #return decoded_jwt['user_id']
 
 # Used to replace list comprehensions when filtering the data store
-def filter_data_store(list, key=None, value=None):
+def filter_data_store(store, key=None, value=None):
     store = data_store.get()
 
     if value is not None and key is not None:
-        filtered_list = [item for item in store[list] if item[key] == value]
+        filtered_list = [item for item in store[store] if (isinstance(item[key], list) and value in item[key]) or item[key] == value]
         if len(filtered_list) == 0:
             return None
         return filtered_list
         
     elif key is not None:
-        return [item[key] for item in store[list]]
+        return [item[key] for item in store[store]]
 
-    return [item for item in store[list]]
+    return [item for item in store[store]]
