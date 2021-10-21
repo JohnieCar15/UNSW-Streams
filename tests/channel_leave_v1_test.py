a307be488f4a2727431a222bd3b8201b60979fe0
Lawrence Chau <z5360392@ad.unsw.edu.au>
2021-10-21 14:03:17 +1100

    Changed from params to json

--- tests/channel_leave_v1_test.py
+++ tests/channel_leave_v1_test.py
@@ -8,11 +8,11 @@ from src.error import InputError, AccessError
 def clear_and_register():
     # clear then register a user 
     requests.delete(config.url + 'clear/v2')
-    register = requests.post(config.url + 'auth/register/v2', params={'email': "yes@yes.com", 'password': "aaaaaa", 'name_first': "firstname", "name_last": "lastname"})
+    register = requests.post(config.url + 'auth/register/v2', json={'email': "yes@yes.com", 'password': "aaaaaa", 'name_first': "firstname", "name_last": "lastname"})
     register_data = register.json()
 
     # create a channel and get its channel id
-    channel_create = requests.post(config.url + 'channels/create/v2', params={'token': token, 'name': "name", 'is_public': True})
+    channel_create = requests.post(config.url + 'channels/create/v2', json={'token': token, 'name': "name", 'is_public': True})
     channel_create_data = channel_create.json()
     channel_create_data["channel_id"]
 
@@ -23,40 +23,40 @@ def clear_and_register():
 def test_valid_channel_authorised(clear_and_register):
     token = clear_and_register['token']
     channel_id = clear_and_register['channel_id']
-    channel_leave = requests.post(config.url + 'channel/leave/v1', params={'token': token, 'channel_id': channel_id})
+    channel_leave = requests.post(config.url + 'channel/leave/v1', json={'token': token, 'channel_id': channel_id})
     channel_leave_data = channel_leave.json()
     assert channel_leave_data == {}
-    channel_details = requests.get(config.url + 'channel/details/v1', params={'token': token, 'channel_id': channel_id})
+    channel_details = requests.get(config.url + 'channel/details/v1', json={'token': token, 'channel_id': channel_id})
     assert channel_details.status_code == AccessError.code
 
 
 # testing a valid channel calleed by an unauthorised member
 def test_valid_channel_unauthorised(clear_and_register):
     channel_id = clear_and_register['channel_id']
-    register = requests.post(config.url + 'auth/register/v2', params={'email': "yes2@yes.com", 'password': "aaaaaa", 'name_first': "name", "name_last": "name"})
+    register = requests.post(config.url + 'auth/register/v2', json={'email': "yes2@yes.com", 'password': "aaaaaa", 'name_first': "name", "name_last": "name"})
     register_data = register.json()
     token_2 = register_data['token']
-    channel_leave = requests.post(config.url + 'channel/leave/v1', params={'token': token_2, 'channel_id': channel_id})
+    channel_leave = requests.post(config.url + 'channel/leave/v1', json={'token': token_2, 'channel_id': channel_id})
     assert channel_leave.status_code == AccessError.code
 
 # testing a valid channel called by an invalid id 
 def test_valid_channel_invalid_token(clear_and_register):
     token = clear_and_register['token']
     channel_id = clear_and_register['channel_id']
-    channel_leave = requests.post(config.url + 'channel/leave/v1', params={'token': token + 1, 'channel_id': channel_id})
+    channel_leave = requests.post(config.url + 'channel/leave/v1', json={'token': token + 1, 'channel_id': channel_id})
     assert channel_leave.status_code == AccessError.code
 
 # testing an invalid channel called by valid token
 def test_invalid_channel_valid_token():
     requests.delete(config.url + 'clear/v2')
-    register = requests.post(config.url + 'auth/register/v2', params={'email': "yes@yes.com", 'password': "aaaaaa", 'name_first': "firstname", "name_last": "lastname"})
+    register = requests.post(config.url + 'auth/register/v2', json={'email': "yes@yes.com", 'password': "aaaaaa", 'name_first': "firstname", "name_last": "lastname"})
     register_data = register.json()
     token = register_data['token']
-    channel_leave = requests.post(config.url + 'channel/leave/v1', params={'token': token, 'channel_id': 1})
+    channel_leave = requests.post(config.url + 'channel/leave/v1', json={'token': token, 'channel_id': 1})
     assert channel_leave.status_code == InputError.code
 
 # testing an invalid channel called by invalid token
 def test_invalid_channel_invalid_token():
     requests.delete(config.url + 'clear/v2')
-    channel_leave = requests.post(config.url + 'channel/leave/v1', params]{'token': 1, 'channel_id': 1})
+    channel_leave = requests.post(config.url + 'channel/leave/v1', json={'token': 1, 'channel_id': 1})
     assert channel_leave.status_code == AccessError.code
