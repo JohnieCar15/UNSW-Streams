77d765973d5f3923fbb8218781f00e63de6d60bb
Lawrence Chau <z5360392@ad.unsw.edu.au>
2021-10-21 21:33:17 +1100

    Updated tests for channel_addowner_v1

--- tests/channel_addowner_v1_test.py
+++ tests/channel_addowner_v1_test.py
@@ -2,47 +2,40 @@ import pytest
 import requests
 import json
 from src import config
-from src.other import clear_v2
-from src.auth import auth_register_v2
-from src.channels import channels_create_v2
-from src.channel import channel_details_v2, import channel_addowner_v1
+from src.error import InputError, AccessError
+
 
 @pytest.fixture
 def clear_and_channel_2_members():
     
     requests.delete(config.url + 'clear/v2')
-    register = requests.post(config.url + 'auth/register/v2', params={'email': "yes@yes.com", 'password': "aaaaaa", 'name_first': "firstname", "name_last": "lastname"})
+    register = requests.post(config.url + 'auth/register/v2', json={'email': "yes@yes.com", 'password': "aaaaaa", 'name_first': "firstname", "name_last": "lastname"})
     register_data = register.json()
     token = register_data['token']
 
-    channel_create = requests.post(config.url + 'channels/create/v2', params={'token': token, 'name': 'name', 'is_public': True})
+    channel_create = requests.post(config.url + 'channels/create/v2', json={'token': token, 'name': 'name', 'is_public': True})
     channel_create_data = channel_create.json()
     channel_id = channel_create_data['channel_id']
 
-    register_2 = requests.post(config.url + 'auth/register/v2', params={'email': "yes2@yes.com", 'password': "aaaaaa", 'name_first': "name", "name_last": "name"})
-    register_2_data = register.json()
+    register_2 = requests.post(config.url + 'auth/register/v2', json={'email': "yes2@yes.com", 'password': "aaaaaa", 'name_first': "name", "name_last": "name"})
+    register_2_data = register_2.json()
     u_id_2 = register_2_data['auth_user_id']
 
-    channel_invite = requests.post(config.url + 'channel/invite/v2', params ={'token': token, 'channel_id': channel_id, 'u_id': u_id_2})
+    channel_invite = requests.post(config.url + 'channel/invite/v2', json ={'token': token, 'channel_id': channel_id, 'u_id': u_id_2})
     
 
     return {'token': token, 'token_2': register_2_data['token'], 'u_id': register_data['auth_user_id'], 'u_id_2': u_id_2, 'channel_id': channel_id}
 
-@pytest.fixture
-def channel_details_for_owner():
-    channel_details = requests.get(config.url + 'channel/details/v2', params={'token': token, 'channel_id': channel_id})
-    channel_details_data = channel_details.json()
-    return channel_details_data
-
 # when token is valid, channel is valid, called by a channel owner 
-def test_valid_channel_channel_owner(clear_and_channel_2_members, channel_details_for_owner):
+def test_valid_channel_channel_owner(clear_and_channel_2_members):
     token = clear_and_channel_2_members['token']
     u_id = clear_and_channel_2_members['u_id']
     u_id_2 = clear_and_channel_2_members['u_id_2']
     channel_id = clear_and_channel_2_members['channel_id']
 
-    channel_addowner = requests.post(config.url + 'channel/addowner/v1', params={'token': token, 'channel_id': channel_id, 'u_id': u_id_2})
-    channel_details_data = channel_details_for_owner
+    requests.post(config.url + 'channel/addowner/v1', json={'token': token, 'channel_id': channel_id, 'u_id': u_id_2})
+    channel_details = requests.get(config.url + 'channel/details/v2', params={'token': token, 'channel_id': channel_id})
+    channel_details_data = channel_details.json()
     assert channel_details_data['owner_members'] == [
             {
                 'u_id': u_id,
@@ -67,23 +60,23 @@ def test_valid_channel_channel_owner(clear_and_channel_2_members, channel_detail
 # when token is valid but channel id is invalid
 def test_invalid_channel():
     requests.delete(config.url + 'clear/v2')
-    register = requests.post(config.url + 'auth/register/v2', params={'email': "yes@yes.com", 'password': "aaaaaa", 'name_first': "firstname", "name_last": "lastname"})
+    register = requests.post(config.url + 'auth/register/v2', json={'email': "yes@yes.com", 'password': "aaaaaa", 'name_first': "firstname", "name_last": "lastname"})
     register_data = register.json()
     token = register_data['token']
     u_id = register_data['auth_user_id']
-    channel_addowner = requests.post(config.url + 'channel/addowner/v1', params={'token': token, 'channel_id': 1, 'u_id': u_id})
-    assert channel_addowner.status_code == 400
+    channel_addowner = requests.post(config.url + 'channel/addowner/v1', json={'token': token, 'channel_id': 1, 'u_id': u_id})
+    assert channel_addowner.status_code == InputError.code
 
 # when token is valid, channel is valid but user not a member
 def test_invaid_u_id_not_member(clear_and_channel_2_members):
     token = clear_and_channel_2_members['token']
     channel_id = clear_and_channel_2_members['channel_id']
 
-    register = requests.post(config.url + 'auth/register/v2', params={'email': "yes3@yes.com", 'password': "aaaaaa", 'name_first': "name", "name_last": "name"})
+    register = requests.post(config.url + 'auth/register/v2', json={'email': "yes3@yes.com", 'password': "aaaaaa", 'name_first': "name", "name_last": "name"})
     register_data = register.json()
     not_member_id = register_data['auth_user_id']
-    channel_addowner = requests.post(config.url + 'channel/addowner/v1', params={'token': token, 'channel_id': channel_id, 'u_id':not_member_id})
-    assert channel_addowner.status_code == 400
+    channel_addowner = requests.post(config.url + 'channel/addowner/v1', json={'token': token, 'channel_id': channel_id, 'u_id':not_member_id})
+    assert channel_addowner.status_code == InputError.code 
     
 # when token is valid, channel is valid but user_id not a real user 
 def test_invalid_u_id_not_user(clear_and_channel_2_members):
@@ -95,24 +88,24 @@ def test_invalid_u_id_not_user(clear_and_channel_2_members):
     invalid_u_id = 1
     while (invalid_u_id == u_id or invalid_u_id == u_id_2):
         invalid_u_id += 1
-    channel_addowner = requests.post(config.url + 'channel/addowner/v1', params={'token': token, 'channel_id': channel_id, 'u_id': invalid_u_id})
-    assert channel_addowner.status_code == 400
+    channel_addowner = requests.post(config.url + 'channel/addowner/v1', json={'token': token, 'channel_id': channel_id, 'u_id': invalid_u_id})
+    assert channel_addowner.status_code == InputError.code
 
 # when token is valid, channel is valid but user_id is already channel owner
 def test_already_channel_owner(clear_and_channel_2_members):
     token = clear_and_channel_2_members['token']
     u_id = clear_and_channel_2_members['u_id']
     channel_id = clear_and_channel_2_members['channel_id']
-    channel_addowner = requests.post(config.url + 'channel/addowner/v1', params={'token': token, 'channel_id': channel_id, 'u_id': u_id})
-    assert channel_addowner.status_code == 400
+    channel_addowner = requests.post(config.url + 'channel/addowner/v1', json={'token': token, 'channel_id': channel_id, 'u_id': u_id})
+    assert channel_addowner.status_code == InputError.code
 
 # valid token, valid channel, valid u_id, no owner permissions 
 def test_no_owner_permissions(clear_and_channel_2_members):
     token_2 = clear_and_channel_2_members['token_2']
-    u_id = clear_and_channel_2_members['u_id']
+    u_id_2 = clear_and_channel_2_members['u_id_2']
     channel_id = clear_and_channel_2_members['channel_id']
-    channel_addowner = requests.post(config.url + 'channel/addowner/v1', params={'token': token_2, 'channel_id': channel_id, 'u_id': u_id})
-    assert channel_addowner.status_code == 403
+    channel_addowner = requests.post(config.url + 'channel/addowner/v1', json={'token': token_2, 'channel_id': channel_id, 'u_id': u_id_2})
+    assert channel_addowner.status_code == AccessError.code
 
 # invalid token, valid channel, valid u_id
 def test_no_owner_permissions(clear_and_channel_2_members):
@@ -125,8 +118,8 @@ def test_no_owner_permissions(clear_and_channel_2_members):
     while (invalid_token == token or invalid_token == token_2):
         invalid_token += 1
 
-    channel_addowner = requests.post(config.url + 'channel/addowner/v1', params={'token': invalid_token, 'channel_id': channel_id, 'u_id': u_id_2})
-    assert channel_addowner.status_code == 403
+    channel_addowner = requests.post(config.url + 'channel/addowner/v1', json={'token': invalid_token, 'channel_id': channel_id, 'u_id': u_id_2})
+    assert channel_addowner.status_code == AccessError.code
 
 # invaid token, invalid channel 
 def test_no_owner_permissions_invalid_channel(clear_and_channel_2_members):
@@ -143,5 +136,5 @@ def test_no_owner_permissions_invalid_channel(clear_and_channel_2_members):
     while (invalid_channel_id == channel_id):
         invalid_token += 1
 
-    channel_addowner = requests.post(config.url + 'channel/addowner/v1', params={'token': invalid_token, 'channel_id': invalid_channel_id, 'u_id': u_id_2})
-    assert channel_addowner.status_code == 403
+    channel_addowner = requests.post(config.url + 'channel/addowner/v1', json={'token': invalid_token, 'channel_id': invalid_channel_id, 'u_id': u_id_2})
+    assert channel_addowner.status_code == AccessError.code
