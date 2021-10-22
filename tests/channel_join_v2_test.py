adb6f47022b08ed0f334b4a6a91416320ba024f0
Krishen T <z5362990@unsw.edu.au>
2021-10-22 12:44:23 +1100

    Fixed channel_join_v2_test style and fixed tokenisation implementation

--- tests/channel_join_v2_test.py
+++ tests/channel_join_v2_test.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
-import json
 from src import config
+from src.error import AccessError, InputError
 
 
 @pytest.fixture
@@ -12,79 +12,85 @@ def channel_join_url():
 def clear_and_register():
     requests.delete(config.url + 'clear/v1')
 
-    user1_input = {
+    auth_register_input1 = {
         'email':'user1@gmail.com',
         'password':'password',
         'name_first':'First',
         'name_last': 'Last'
     }
-    user1_dict = requests.post(config.url + 'auth/register/v2', json=user1_input).json()
-    
-    user2_input = {
+    user1_payload = requests.post(config.url + 'auth/register/v2', json=auth_register_input1).json()
+
+    auth_register_input2 = {
         'email':'user2@gmail.com',
         'password':'password',
         'name_first':'First',
         'name_last': 'Last'
     }
-    user2_dict = requests.post(config.url + 'auth/register/v2', json=user2_input).json()
-    
-    return {'user1_id': user1_dict['auth_user_id'], 'user2_id': user2_dict['auth_user_id']}
+    user2_payload = requests.post(config.url + 'auth/register/v2', json=auth_register_input2).json()
+
+    return {
+        'user1_token': user1_payload['token'],
+        'user1_id': user1_payload['auth_user_id'],
+        'user2_token': user2_payload['token'],
+        'user2_id': user2_payload['auth_user_id']
+        }
 
 # Testing the general case of a user joining a public channel
-def test_general(channel_join_url, clear_and_register):
-    creator_id = clear_and_register['user1_id']
+def test_join_public(channel_join_url, clear_and_register):
+    creator_token = clear_and_register['user1_token']
+    user_token = clear_and_register['user2_token']
     user_id = clear_and_register['user2_id']
+
     channels_create_input = {
-        'token': str(creator_id),
+        'token': creator_token,
         'name': 'Channel',
         'is_public': True
     }
     channel_id = requests.post(config.url + 'channels/create/v2',json=channels_create_input).json()['channel_id']
     
     channels_join_input = {
-        'token': str(user_id),
+        'token': user_token,
         'channel_id': channel_id,
     }
-    requests.post(channel_join_url, json=channels_join_input)
+    r = requests.post(channel_join_url, json=channels_join_input)
 
     channel_details_input = {
-        'token': str(creator_id),
+        'token': creator_token,
         'channel_id': channel_id
     }
     channel_members = requests.get(config.url + 'channel/details/v2',params=channel_details_input).json()['all_members']
     channel_members_ids = [user['u_id'] for user in channel_members]
 
-    assert user_id in channel_members_ids
+    assert r.status_code == 200 and user_id in channel_members_ids
 
-# Testing the error case of passing an invalid auth_user_id
-def test_invalid_user_id(channel_join_url, clear_and_register):
-    valid_user_id = clear_and_register['user1_id']
+# Testing the error case of passing an invalid token
+def test_invalid_token(channel_join_url, clear_and_register):
+    valid_user_token = clear_and_register['user1_token']
+
     channels_create_input = {
-        'token': str(valid_user_id),
+        'token': valid_user_token,
         'name': 'Channel',
         'is_public': True
     }
     valid_channel_id = requests.post(config.url + 'channels/create/v2',json=channels_create_input).json()['channel_id']
 
-    # Generating an invalid id that doesn't match existing ids
-    invalid_user_id = valid_user_id + 1
-    if invalid_user_id == clear_and_register['user2_id']:
-        invalid_user_id += 1
+    # Creating an invalid token (empty string)
+    invalid_auth_token = ''
 
     channels_join_input = {
-        'token': str(invalid_user_id),
+        'token': invalid_auth_token,
         'channel_id': valid_channel_id
     }
     r = requests.post(channel_join_url, json=channels_join_input)
     
-    #Throws AccessError
-    assert r.status_code == 403
+    assert r.status_code == AccessError.code
 
 # Testing the error case of passing an invalid channel_id
 def test_invalid_channel_id(channel_join_url, clear_and_register):
-    valid_user_id = clear_and_register['user1_id']
+    valid_user_token = clear_and_register['user1_token']
+
     channels_create_input = {
-        'token': str(valid_user_id),
+        'token': valid_user_token,
         'name': 'Channel',
         'is_public': True
     }
@@ -94,104 +100,102 @@ def test_invalid_channel_id(channel_join_url, clear_and_register):
     invalid_channel_id = valid_channel_id + 1
 
     channels_join_input = {
-        'token': str(valid_user_id),
+        'token': valid_user_token,
         'channel_id': invalid_channel_id
     }
     r = requests.post(channel_join_url, json=channels_join_input)
     
-    #Throws InputError
-    assert r.status_code == 400
+    assert r.status_code == InputError.code
 
-# Testing the error case of passing an invalid auth_user_id and channel_id
+# Testing the error case of passing an invalid token and channel_id
 def test_all_ids_invalid(channel_join_url, clear_and_register):
-    valid_user_id = clear_and_register['user1_id']
+    valid_user_token = clear_and_register['user1_id']
 
     channels_create_input = {
-        'token': str(valid_user_id),
+        'token': valid_user_token,
         'name': 'Channel',
         'is_public': True
     }
     valid_channel_id = requests.post(config.url + 'channels/create/v2',json=channels_create_input).json()['channel_id']
 
-    # Generating invalid ids that don't match exising ids
+    # Creating an invalid token (empty string)
+    invalid_auth_token = ''
+    # Generating invalid channel id that doesn't match any exising ids
     invalid_channel_id = valid_channel_id + 1
-    invalid_user_id = valid_user_id + 1
-    if invalid_user_id == clear_and_register['user2_id']:
-        invalid_user_id += 1
 
     channels_join_input = {
-        'token': str(invalid_user_id),
+        'token': invalid_auth_token,
         'channel_id': invalid_channel_id
     }
     r = requests.post(channel_join_url, json=channels_join_input)
 
-    #Throws AccessError
-    assert r.status_code == 403
+    assert r.status_code == AccessError.code
 
 # Testing the error case of a user joining a channel again
 def test_duplicate(channel_join_url, clear_and_register):
-    user_id = clear_and_register['user1_id']
+    user_token = clear_and_register['user1_token']
 
     channels_create_input = {
-        'token': str(user_id),
+        'token': user_token,
         'name': 'Channel',
         'is_public': True
     }
     channel_id = requests.post(config.url + 'channels/create/v2',json=channels_create_input).json()['channel_id']
 
     channels_join_input = {
-        'token': str(user_id),
+        'token': user_token,
         'channel_id': channel_id
     }
     r = requests.post(channel_join_url, json=channels_join_input)
     
-    #Throws InputError
-    assert r.status_code == 400
+    assert r.status_code == InputError.code
 
 # Testing the error case of a non-global owner joining a private channel
 def test_private_channel_without_global_owner(clear_and_register):
-    creator_id = clear_and_register['user1_id']
-    user_id = clear_and_register['user2_id']
+    creator_token = clear_and_register['user1_token']
+    user_token = clear_and_register['user2_token']
+
     channels_create_input = {
-        'token': str(creator_id),
+        'token': creator_token,
         'name': 'Channel',
         'is_public': False
     }
     private_channel_id = requests.post(config.url + 'channels/create/v2',json=channels_create_input).json()['channel_id']
 
     channels_join_input = {
-        'token': str(user_id),
+        'token': user_token,
         'channel_id': private_channel_id
     }
     r = requests.post(channel_join_url, json=channels_join_input)
     
-    #Throws AccessError
-    assert r.status_code == 403
+    assert r.status_code == AccessError.code
 
 
 
 # Testing the general case of a global owner joining a private channel
 def test_private_channel_with_global_owner(clear_and_register):
+    creator_token = clear_and_register['user2_token']
+    global_owner_token = clear_and_register['user1_token']
     global_owner_id = clear_and_register['user1_id']
-    creator_id = clear_and_register['user2_id']
+
     channels_create_input = {
-        'token': str(creator_id),
+        'token': creator_token,
         'name': 'Channel',
         'is_public': False
     }
     private_channel_id = requests.post(config.url + 'channels/create/v2',json=channels_create_input).json()['channel_id']
 
     channels_join_input = {
-        'token': str(global_owner_id),
+        'token': global_owner_token,
         'channel_id': private_channel_id
     }
-    requests.post(channel_join_url, json=channels_join_input)
+    r = requests.post(channel_join_url, json=channels_join_input)
 
     channel_details_input = {
-        'token': str(creator_id),
+        'token': creator_token,
         'channel_id': private_channel_id
     }
     channel_members = requests.get(config.url + 'channel/details/v2',params=channel_details_input).json()['all_members']
     channel_members_ids = [user['u_id'] for user in channel_members]
 
-    assert global_owner_id in channel_members_ids
+    assert r.status_code == 200 and global_owner_id in channel_members_ids
