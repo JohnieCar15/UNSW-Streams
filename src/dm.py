79fcb128e88f306538810454b14d4d3ba86d7e2f
Dylan Beretov <z5359599@ad.unsw.edu.au>
2021-10-23 13:16:44 +1100

    Fixed coverage of dm.py

--- src/dm.py
+++ src/dm.py
@@ -37,50 +37,6 @@ def dm_create_v1(token, u_ids):
         'dm_id': new_id
     }
 
-def dm_leave_v1(token, dm_id):
-    store = data_store.get()
-
-    auth_user_id = validate_token(token)['user_id']
-
-    found = False
-
-    for dm in store['dms']:
-        if dm['dm_id'] == dm_id:
-            found = True
-            if auth_user_id not in dm['members']:
-                raise AccessError(description='User is not a member of the DM')
-            else:
-                dm['members'].remove(auth_user_id)
-
-    if not found:
-        raise InputError(description='dm_id does not refere to valid DM')
-
-    data_store.set(store)
-    
-    return {}
-
-def dm_remove_v1(token, dm_id):
-    store = data_store.get()
-
-    auth_user_id = validate_token(token)['user_id']
-
-    found = False
-
-    for dm in store['dms']:
-        if dm['dm_id'] == dm_id:
-            found = True
-            if dm['owner'] != auth_user_id:
-                raise AccessError(description='User is not the owner of the DM')
-            else:
-                dm['members'] = []
-
-    if not found:
-        raise InputError(description='dm_id does not refere to valid DM')
-
-    data_store.set(store)
-    
-    return {}
-
 def dm_list_v1(token):
     store = data_store.get()
 
--- tests/dm_list_v1_test.py
+++ tests/dm_list_v1_test.py
@@ -4,7 +4,8 @@ import requests
 from src import config
 from src.error import AccessError
 
-def test_dm_list_v1():
+@pytest.fixture
+def clear_register_create_dm():
     requests.delete(config.url + '/clear/v1')
 
     auth_register_input = {
@@ -14,24 +15,22 @@ def test_dm_list_v1():
         'name_last':'Last'
     }
 
-    token_1 = requests.post(config.url + 'auth/register/v2', json=auth_register_input).json()['token']
+    token = requests.post(config.url + 'auth/register/v2', json=auth_register_input).json()['token']
 
-    auth_register_input = {
-        'email':'second@gmail.com',
-        'password':'password',
-        'name_first':'First',
-        'name_last':'Last'
+    dm_id = requests.post(config.url + 'dm/create/v1', json={'token': token, 'u_ids': []}).json()['dm_id']
+
+    return {
+        'token': token,
+        'dm_id': dm_id
     }
 
-    user_2 = requests.post(config.url + 'auth/register/v2', json=auth_register_input).json()
-    token_2 = user_2['token']
-    u_id_2 = user_2['auth_user_id']
 
-    dm_id = requests.post(config.url + 'dm/create/v1', json={'token': token_1, 'u_ids': [u_id_2]}).json()['dm_id']
+def test_dm_list_v1(clear_register_create_dm):
+    token = clear_register_create_dm['token']
 
-    dm_list = requests.get(config.url + 'dm/list/v1', params={'token': token_1}).json()['dms']
+    dm_list = requests.get(config.url + 'dm/list/v1', params={'token': token}).json()['dms']
 
-    assert dm_list[0]['dm_id'] == dm_id
+    assert dm_list[0]['dm_id'] == clear_register_create_dm['dm_id']
 
 def test_invalid_token():
     requests.delete(config.url + '/clear/v1')
@@ -39,3 +38,17 @@ def test_invalid_token():
     list_return = requests.get(config.url + 'dm/list/v1', params={'token': ''})
 
     assert list_return.status_code == AccessError.code
+
+def test_empty_dm_list(clear_register_create_dm):
+    auth_register_input = {
+        'email':'second@gmail.com',
+        'password':'password',
+        'name_first':'First',
+        'name_last':'Last'
+    }
+
+    token_2 = requests.post(config.url + 'auth/register/v2', json=auth_register_input).json()['token']
+
+    dm_list = requests.get(config.url + 'dm/list/v1', params={'token': token_2}).json()['dms']
+
+    assert dm_list == [] 
