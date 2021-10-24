ddd4040df9301272662b487db1baf078fa333358
Haokai <haokai.yin@student.unsw.edu.au>
2021-10-24 23:29:46 +1100

    Fixed code style of user functions and tests

--- src/channels.py
+++ src/channels.py
@@ -69,7 +69,7 @@ def channels_list_v2(token):
 
     # Checking if token is valid
     # if token can not be decoded
-    # raise AccessError("Invalid token")
+    # raise AccessError(description="Invalid token")
     auth_user_id = validate_token(token)['user_id']
    
     list_of_channel = filter_data_store(store_list='channels', key='members', value=auth_user_id)
@@ -100,7 +100,7 @@ def channels_listall_v2(token):
 
     # Checking if token is valid
     # if token can not be decoded
-    # raise AccessError("Invalid token")
+    # raise AccessError(description="Invalid token")
     validate_token(token)['user_id']
     
     # initilise the returned list
--- tests/users_all_v1_test.py
+++ tests/users_all_v1_test.py
@@ -9,35 +9,35 @@ def clear_and_register_user0():
     requests.delete(config.url + 'clear/v1')
 
     user0_register = {
-        "email" : "0000@unsw.edu.au",
-        "password" : "password",
-        "name_first" : "firstname0",
-        "name_last" : "lastname0",
+        'email' : '0000@unsw.edu.au',
+        'password' : 'password',
+        'name_first' : 'firstname0',
+        'name_last' : 'lastname0',
     }
     user0 = requests.post(config.url + 'auth/register/v2', json=user0_register).json()
     
     return {
-        "token_valid": user0['token'],
-        "u_id": user0['auth_user_id']
+        'token_valid': user0['token'],
+        'u_id': user0['auth_user_id']
     }
 
 # test invalid token and this should raise AccessError
 def test_invalid_token(clear_and_register_user0):
     user0 = clear_and_register_user0
-    input_token = {"token": user0['token_valid'] + "1"}
+    input_token = {'token': user0['token_valid'] + '1'}
     assert requests.get(config.url + 'users/all/v1', params=input_token).status_code == AccessError.code
 
 
 def test_vaild_token_with_one_user_registered(clear_and_register_user0):
     user0 = clear_and_register_user0
-    input_token = {"token": user0['token_valid']}
+    input_token = {'token': user0['token_valid']}
     assert requests.get(config.url + 'users/all/v1', params=input_token).json() == {
         'users':[
             {'u_id': user0['u_id'], 
-            'email': "0000@unsw.edu.au", 
-            "name_first": "firstname0", 
-            "name_last": "lastname0",
-            "handle_str": "firstname0lastname0"}
+            'email': '0000@unsw.edu.au', 
+            'name_first': 'firstname0', 
+            'name_last': 'lastname0',
+            'handle_str': 'firstname0lastname0'}
         ]
     } 
 
@@ -45,18 +45,18 @@ def test_vaild_token_with_one_user_registered(clear_and_register_user0):
 def test_vaild_token_with_many_user_registered():
     requests.delete(config.url + 'clear/v1')
     users_register = [
-        {   "email" : "0000@unsw.edu.au",
-            "password" : "password",
-            "name_first" : "firstname0",
-            "name_last" : "lastname0",
-        }, {"email" : "0001@unsw.edu.au",
-            "password" : "password",
-            "name_first" : "firstname1",
-            "name_last" : "lastname1",
-        }, {"email" : "0002@unsw.edu.au",
-            "password" : "password",
-            "name_first" : "firstname2",
-            "name_last" : "lastname2",
+        {   'email' : '0000@unsw.edu.au',
+            'password' : 'password',
+            'name_first' : 'firstname0',
+            'name_last' : 'lastname0',
+        }, {'email' : '0001@unsw.edu.au',
+            'password' : 'password',
+            'name_first' : 'firstname1',
+            'name_last' : 'lastname1',
+        }, {'email' : '0002@unsw.edu.au',
+            'password' : 'password',
+            'name_first' : 'firstname2',
+            'name_last' : 'lastname2',
         }
     ]
     
@@ -64,25 +64,25 @@ def test_vaild_token_with_many_user_registered():
     user1 = requests.post(config.url + 'auth/register/v2', json=users_register[1]).json()
     user2 = requests.post(config.url + 'auth/register/v2', json=users_register[2]).json()
     
-    input_token = {"token": user0['token']}
+    input_token = {'token': user0['token']}
     result = {
         'users': [
             {'u_id': user0['auth_user_id'],
-            'email': "0000@unsw.edu.au",
-            "name_first": "firstname0",
-            "name_last": "lastname0",
-            "handle_str": "firstname0lastname0"},
+            'email': '0000@unsw.edu.au',
+            'name_first': 'firstname0',
+            'name_last': 'lastname0',
+            'handle_str': 'firstname0lastname0'},
 
             {'u_id': user1['auth_user_id'], 
-            'email': "0001@unsw.edu.au", 
-            "name_first": "firstname1", 
-            "name_last": "lastname1",
-            "handle_str": "firstname1lastname1"},
+            'email': '0001@unsw.edu.au', 
+            'name_first': 'firstname1', 
+            'name_last': 'lastname1',
+            'handle_str': 'firstname1lastname1'},
 
             {'u_id': user2['auth_user_id'], 
-            'email': "0002@unsw.edu.au", 
-            "name_first": "firstname2", 
-            "name_last": "lastname2",
-            "handle_str": "firstname2lastname2"}
+            'email': '0002@unsw.edu.au', 
+            'name_first': 'firstname2', 
+            'name_last': 'lastname2',
+            'handle_str': 'firstname2lastname2'}
     ]}
     assert requests.get(config.url + 'users/all/v1', params=input_token).json() == result
