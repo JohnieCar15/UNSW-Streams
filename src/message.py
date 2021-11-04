684886f673ed52c861aa4213d9f9ad59b810e9d7
Johnie Carino <z5361576@ad.unsw.edu.au>
2021-11-04 22:15:34 +1100

    Modifications made to message send and message senddm for iteration 3

--- src/message.py
+++ src/message.py
@@ -109,7 +109,13 @@ def message_send_v1(token, channel_id, message):
         'message_id': len(store['messages']) + len(store['removed_messages']) + 1,
         'u_id': auth_user_id,
         'message': message,
-        'time_created': int(datetime.utcnow().timestamp())
+        'time_created': int(datetime.utcnow().timestamp()),
+        'reacts' : {
+            'react_id' : 1,
+            'u_ids' : [],
+            'is_this_user_reacted' : False
+        },
+        'is_pinned' : False
     }
 
     # Data store creates extra field of channel id for easier identification
@@ -173,7 +179,13 @@ def message_senddm_v1(token, dm_id, message):
         'message_id': len(store['messages']) + len(store['removed_messages']) + 1,
         'u_id': auth_user_id,
         'message': message,
-        'time_created': int(datetime.utcnow().timestamp())
+        'time_created': int(datetime.utcnow().timestamp()),
+        'reacts' : {
+            'react_id' : 1,
+            'u_ids' : [],
+            'is_this_user_reacted' : False
+        },
+        'is_pinned' : False
     }
 
     # Data store creates extra field of channel id for easier identification
