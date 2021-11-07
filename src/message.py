9d6fcd4f0f0cf7243b18ba5b9fc4f72e2985e97e
Johnie Carino <z5361576@ad.unsw.edu.au>
2021-11-07 21:23:32 +1100

    Implemented message share, endpoint and test

--- src/message.py
+++ src/message.py
@@ -1,3 +1,4 @@
+from src import auth, channel
 from src.data_store import data_store
 from src.error import InputError, AccessError
 from src.helpers import is_global_owner, validate_token, filter_data_store
@@ -244,3 +245,73 @@ def message_remove_v1(token, message_id):
 
     return {}
     
+def message_share_v1(token, og_message_id, message, channel_id, dm_id):
+    store = data_store.get()
+
+    # check if token is valid
+    auth_user_id = validate_token(token)['user_id']
+
+    # Checks if either channel_id or dm_id is equal to -1
+    if (channel_id == -1 and dm_id == -1) or (channel_id != -1 and dm_id != -1):
+        raise InputError(description="only channel id or dm id can be equal to -1")
+    
+    if dm_id == -1:
+        # Checks if channel id is valid
+        if channel_id not in filter_data_store(store_list='channels', key='id', value=None):
+            raise InputError(description="Invalid channel_id")
+        final_id = channel_id
+    else:
+        # Checks if dm id is valid
+        if dm_id not in filter_data_store(store_list='dms', key='id', value=None):
+            raise InputError(description="Invalid dm_id")
+        final_id = dm_id
+
+    # Looks for the channel/dm this message is being sent to
+    shared_channel_dict = [channel for channel in (store['channels'] + store['dms']) if final_id == channel['id']][0]
+
+    # Check if the user is part of the channel/dm the message is being sent to 
+    if auth_user_id not in shared_channel_dict['members']:
+        raise AccessError(description="Not a member of channel")
+
+    # Checks if message exists
+    # Contains extra field "channel_id"
+    look_message = [message for message in store['messages'] if message['message']['message_id'] == og_message_id]
+
+    if not look_message:
+        raise InputError(description="Invalid message")
+    else:
+        messagedict = look_message[0]
+
+    # Checks which channel/dm the message is sent in and finds that message
+    og_channel_dict = [channel for channel in (store['channels'] + store['dms']) if messagedict['channel_id'] == channel['id']][0]
+    og_selected_message = [message for message in og_channel_dict['messages'] if message['message_id'] == og_message_id][0]
+
+    # Checks if user is part of that channel
+    if auth_user_id not in og_channel_dict['members']:
+        raise InputError(description="Invalid message")
+
+    if len(message) > 1000:
+        raise InputError(description="Invalid message length")
+
+    # Sets up new keys for new message
+    new_message = {
+        'message_id': len(store['messages']) + len(store['removed_messages']) + 1,
+        'u_id': auth_user_id,
+        'message': f"{message}" + "\n\n" + f'"""\n{og_selected_message["message"]}\n"""',
+        'time_created': int(datetime.utcnow().timestamp())
+    }
+
+    # Data store creates extra field of channel id for easier identification
+    message_store = {
+        'message': new_message,
+        'channel_id': final_id
+    }
+
+    shared_channel_dict['messages'].insert(0, new_message)
+    store['messages'].insert(0, message_store)
+
+    data_store.set(store)
+
+    return {
+        'shared_message_id' : new_message['message_id']
+    }
--- tests/message_share_v1_test.py
+++ tests/message_share_v1_test.py
@@ -1,3 +1,4 @@
+from requests.api import get
 import pytest
 import requests
 from src import config
@@ -44,27 +45,27 @@ def register_create_dm():
         'name_last' : "Last",
     }
 
-    user_id = requests.post(config.url + 'auth/register/v2', json=auth_register_input).json()
+    user_id = requests.post(config.url + '/auth/register/v2', json=auth_register_input).json()
 
     dm_create_input = {
         'token' : user_id['token'],
         'u_ids' : []
     }
 
-    dm_id = requests.post(config.url + 'dm/create/v1', json=dm_create_input).json()['dm_id']
+    dm_id = requests.post(config.url + '/dm/create/v1', json=dm_create_input).json()['dm_id']
 
     return {
-        'valid_token_owner': user_id['token'], 
+        'valid_token': user_id['token'], 
         'valid_user_id': user_id['auth_user_id'], 
         'valid_dm_id': dm_id
     }
 
 # HELPER FUNCTION
 # Sends a number of messages to specific endpoint
-def send_message(register_create, length):
+def send_message(token, channel_id, length):
     send_message_input = {
-        'token' : register_create['valid_token'],
-        'channel_id': register_create['valid_channel_id'],
+        'token' : token,
+        'channel_id': channel_id,
         'message': "Hello!"
     }
 
@@ -81,12 +82,33 @@ def send_message(register_create, length):
     }
 
 # HELPER FUNCTION
+# Sends a number of messages to specific endpoint
+def send_messagedm(token, dm_id, length):
+    send_messagedm_input = {
+        'token' : token,
+        'dm_id': dm_id,
+        'message': "Hello!"
+    }
+
+    message_id_list = []
+    timelist = []
+
+    for _ in range (length):
+        timelist.insert(0, int(datetime.utcnow().timestamp()))
+        message_id_list.insert(0, requests.post(config.url + '/message/senddm/v1', json=send_messagedm_input).json()['message_id'])
+    
+    return {
+        'timelist' : timelist,
+        'message_id_list' : message_id_list
+    }
+
+# HELPER FUNCTION
 # Creates input for channel_messages and returns messages
 # Assumes that tests require valid input, otherwise register_create tokens/channel_ids are directly modified
-def get_messages(register_create, start):
+def get_messages(token, channel_id, start):
     channel_messages_input = {
-        'token' : register_create['valid_token'],
-        'channel_id' : register_create['valid_channel_id'],
+        'token' : token,
+        'channel_id' : channel_id,
         'start' : start
     }
 
@@ -94,7 +116,10 @@ def get_messages(register_create, start):
 
     return channel_messages
 
-def test_share_channel(register_create_channel):
+def test_share_channel_to_channel(register_create_channel):
+    '''
+    Tests sharing of message from channel to channel
+    '''
     new_channel_create_input = {
         'token' : register_create_channel['valid_token'],
         'name' : "new_channel",
@@ -103,7 +128,7 @@ def test_share_channel(register_create_channel):
 
     new_channel_id = requests.post(config.url + '/channels/create/v2', json=new_channel_create_input).json()['channel_id']
 
-    messagedict = send_message(register_create_channel, 1)
+    messagedict = send_message(register_create_channel['valid_token'], register_create_channel['valid_channel_id'], 1)
 
     message_share_input = {
         'token': register_create_channel['valid_token'],
@@ -115,29 +140,18 @@ def test_share_channel(register_create_channel):
 
     shared_message_id = requests.post(config.url + 'message/share/v1', json=message_share_input).json()['shared_message_id']
 
-
-    channel_messages_input = {
-        'token' : register_create_channel['valid_token'],
-        'channel_id' : new_channel_id,
-        'start' : 0
-    }
-
-    channel_messages = requests.get(config.url + '/channel/messages/v2', params=channel_messages_input).json()
+    channel_messages = get_messages(register_create_channel['valid_token'], new_channel_id, 0)
 
     assert channel_messages['messages'][0]['message_id'] == shared_message_id
     assert channel_messages['messages'][0]['u_id'] == register_create_channel['valid_user_id']
-    assert channel_messages['messages'][0]['message'] == "Hello!" + "World!"
+    assert channel_messages['messages'][0]['message'] == "World!" + "\n\n" + '"""\nHello!\n"""'
     assert abs((channel_messages['messages'][0]['time_created'] - messagedict['timelist'][0])) < 2
-    assert channel_messages['messages'][0]['reacts'] == { 
-                                                        'react_id': 1,
-                                                        'u_ids': [],
-                                                        'is_this_user_reacted': False,
-                                                }
-    assert channel_messages['messages'][0]['is_pinned'] == False
 
 
-
-def test_share_dm(register_create_dm):
+def test_share_channel_to_dm(register_create_dm):
+    '''
+    Tests sharing of message from channel to dm
+    '''
     new_channel_create_input = {
         'token' : register_create_dm['valid_token'],
         'name' : "new_channel",
@@ -146,17 +160,11 @@ def test_share_dm(register_create_dm):
 
     new_channel_id = requests.post(config.url + '/channels/create/v2', json=new_channel_create_input).json()['channel_id']
 
-    send_message_input = {
-        'token' : register_create_dm['valid_token'],
-        'channel_id': new_channel_id,
-        'message': "Hello!"
-    }
-    timesent = int(datetime.utcnow().timestamp())
-    message_id = requests.post(config.url + '/message/send/v1', json=send_message_input).json()['message_id']
+    messagedict = send_message(register_create_dm['valid_token'], new_channel_id, 1)
 
     message_share_input = {
         'token': register_create_dm['valid_token'],
-        'og_message_id': message_id,
+        'og_message_id': messagedict['message_id_list'][0],
         'message': "World!",
         'channel_id': -1,
         'dm_id': register_create_dm['valid_dm_id']
@@ -165,34 +173,100 @@ def test_share_dm(register_create_dm):
     shared_message_id = requests.post(config.url + 'message/share/v1', json=message_share_input).json()['shared_message_id']
 
     dm_messages_input = {
-        'token' : register_create_dm['valid_token_owner'],
+        'token' : register_create_dm['valid_token'],
         'dm_id' : register_create_dm['valid_dm_id'],
         'start' : 0
     }
 
-    dm_messages = requests.get(config.url + 'dm/messages/v1', params=dm_messages_input)
+    dm_messages = requests.get(config.url + 'dm/messages/v1', params=dm_messages_input).json()
 
     assert dm_messages['messages'][0]['message_id'] == shared_message_id
-    assert dm_messages['messages'][0]['u_id'] == register_create_channel['valid_user_id']
-    assert dm_messages['messages'][0]['message'] == "Hello!" + "World!"
-    assert abs((dm_messages['messages'][0]['time_created'] - timesent)) < 2
-    assert dm_messages['messages'][0]['reacts'] == { 
-                                                        'react_id': 1,
-                                                        'u_ids': [],
-                                                        'is_this_user_reacted': False,
-                                                }
-    assert dm_messages['messages'][0]['is_pinned'] == False
+    assert dm_messages['messages'][0]['u_id'] == register_create_dm['valid_user_id']
+    assert dm_messages['messages'][0]['message'] == "World!" + "\n\n" + '"""\nHello!\n"""'
+    assert abs((dm_messages['messages'][0]['time_created'] - messagedict['timelist'][0])) < 2
 
-def test_invalid_channel_id_diff(register_create_channel):
+def test_share_dm_to_channel(register_create_dm):
+    '''
+    Tests sharing of message from dm to channel
+    '''
+
     new_channel_create_input = {
-        'token' : register_create_channel['valid_token'],
-        'name' : "channel",
+        'token' : register_create_dm['valid_token'],
+        'name' : "new_channel",
         'is_public' : True
     }
 
     new_channel_id = requests.post(config.url + '/channels/create/v2', json=new_channel_create_input).json()['channel_id']
 
-    messagedict = send_message(register_create_channel, 1)
+    messagedict = send_messagedm(register_create_dm['valid_token'], register_create_dm['valid_dm_id'], 1)
+
+    message_share_input = {
+        'token': register_create_dm['valid_token'],
+        'og_message_id': messagedict['message_id_list'][0],
+        'message': "World!",
+        'channel_id': new_channel_id,
+        'dm_id': -1
+    }
+
+    shared_message_id = requests.post(config.url + 'message/share/v1', json=message_share_input).json()['shared_message_id']
+
+    channel_messages = get_messages(register_create_dm['valid_token'], new_channel_id, 0)
+
+    assert channel_messages['messages'][0]['message_id'] == shared_message_id
+    assert channel_messages['messages'][0]['u_id'] == register_create_dm['valid_user_id']
+    assert channel_messages['messages'][0]['message'] == "World!" + "\n\n" + '"""\nHello!\n"""'
+    assert abs((channel_messages['messages'][0]['time_created'] - messagedict['timelist'][0])) < 2
+
+def test_share_dm_to_dm(register_create_dm):
+    '''
+    Tests sharing of message from dm to dm
+    '''
+    new_dm_create_input = {
+        'token' : register_create_dm['valid_token'],
+        'u_ids' : []
+    }
+
+    new_dm_id = requests.post(config.url + '/dm/create/v1', json=new_dm_create_input).json()['dm_id']
+
+    messagedict = send_messagedm(register_create_dm['valid_token'], register_create_dm['valid_dm_id'], 1)
+
+    message_share_input = {
+        'token': register_create_dm['valid_token'],
+        'og_message_id': messagedict['message_id_list'][0],
+        'message': "World!",
+        'channel_id': -1,
+        'dm_id': new_dm_id
+    }
+
+    shared_message_id = requests.post(config.url + 'message/share/v1', json=message_share_input).json()['shared_message_id']
+
+    dm_messages_input = {
+        'token' : register_create_dm['valid_token'],
+        'dm_id' : new_dm_id,
+        'start' : 0
+    }
+
+    dm_messages = requests.get(config.url + 'dm/messages/v1', params=dm_messages_input).json()
+
+    assert dm_messages['messages'][0]['message_id'] == shared_message_id
+    assert dm_messages['messages'][0]['u_id'] == register_create_dm['valid_user_id']
+    assert dm_messages['messages'][0]['message'] == "World!" + "\n\n" + '"""\nHello!\n"""'
+    assert abs((dm_messages['messages'][0]['time_created'] - messagedict['timelist'][0])) < 2
+
+
+def test_invalid_channel_id_diff(register_create_channel):
+    '''
+    Tests where either channel id or dm id are not equal to -1
+    '''
+    new_channel_create_input = {
+        'token' : register_create_channel['valid_token'],
+        'name' : "new_channel",
+        'is_public' : True
+    }
+
+    new_channel_id = requests.post(config.url + '/channels/create/v2', json=new_channel_create_input).json()['channel_id']
+
+    messagedict = send_message(register_create_channel['valid_token'], new_channel_id, 1)
 
     message_share_input = {
         'token': register_create_channel['valid_token'],
@@ -202,11 +276,15 @@ def test_invalid_channel_id_diff(register_create_channel):
         'dm_id': new_channel_id + 1
     }
 
-    status = requests.post(config.url + 'message/share/v1', json=message_share_input).json()
+    status = requests.post(config.url + 'message/share/v1', json=message_share_input)
 
     assert status.status_code == InputError.code 
 
+
 def test_invalid_channel_id_minus_one(register_create_channel):
+    '''
+    Tests where both channel id or dm id are equal to -1
+    '''
     new_channel_create_input = {
         'token' : register_create_channel['valid_token'],
         'name' : "channel",
@@ -215,7 +293,7 @@ def test_invalid_channel_id_minus_one(register_create_channel):
 
     new_channel_id = requests.post(config.url + '/channels/create/v2', json=new_channel_create_input).json()['channel_id']
 
-    messagedict = send_message(register_create_channel, 1)
+    messagedict = send_message(register_create_channel['valid_token'], new_channel_id, 1)
 
     message_share_input = {
         'token': register_create_channel['valid_token'],
@@ -225,11 +303,53 @@ def test_invalid_channel_id_minus_one(register_create_channel):
         'dm_id': -1
     }
 
-    status = requests.post(config.url + 'message/share/v1', json=message_share_input).json()
+    status = requests.post(config.url + 'message/share/v1', json=message_share_input)
 
     assert status.status_code == InputError.code 
 
-def test_invalid_message_id(register_create_channel):
+def test_invalid_channel_id(register_create_dm):
+    '''
+    Tests invalid channel id that does not exists
+    '''
+
+    messagedict = send_messagedm(register_create_dm['valid_token'], register_create_dm['valid_dm_id'], 1)
+
+    message_share_input = {
+        'token': register_create_dm['valid_token'],
+        'og_message_id': messagedict['message_id_list'][0],
+        'message': "World!",
+        'channel_id': register_create_dm['valid_dm_id'] + 1,
+        'dm_id': -1
+    }
+
+    status = requests.post(config.url + 'message/share/v1', json=message_share_input)
+
+    assert status.status_code == InputError.code 
+
+def test_invalid_dm_id(register_create_channel):
+    '''
+    Tests invalid channel id
+    '''
+
+    messagedict = send_message(register_create_channel['valid_token'], register_create_channel['valid_channel_id'], 1)
+
+    message_share_input = {
+        'token': register_create_channel['valid_token'],
+        'og_message_id': messagedict['message_id_list'][0],
+        'message': "World!",
+        'channel_id': -1,
+        'dm_id': register_create_channel['valid_channel_id'] + 1
+    }
+
+    status = requests.post(config.url + 'message/share/v1', json=message_share_input)
+
+    assert status.status_code == InputError.code 
+
+
+def test_invalid_message_id_shared(register_create_channel):
+    '''
+    Tests invalid message id that does not exist
+    '''
     new_channel_create_input = {
         'token' : register_create_channel['valid_token'],
         'name' : "channel",
@@ -238,7 +358,7 @@ def test_invalid_message_id(register_create_channel):
 
     new_channel_id = requests.post(config.url + '/channels/create/v2', json=new_channel_create_input).json()['channel_id']
 
-    messagedict = send_message(register_create_channel, 1)
+    messagedict = send_message(register_create_channel['valid_token'], new_channel_id, 1)
 
     message_share_input = {
         'token': register_create_channel['valid_token'],
@@ -248,11 +368,14 @@ def test_invalid_message_id(register_create_channel):
         'dm_id': -1
     }
 
-    status = requests.post(config.url + 'message/share/v1', json=message_share_input).json()
+    status = requests.post(config.url + 'message/share/v1', json=message_share_input)
 
     assert status.status_code == InputError.code 
 
-def test_invalid_message(register_create_channel):
+def test_invalid_message_id_og(register_create_channel):
+    '''
+    Tests invalid message id from og channel
+    '''
     new_channel_create_input = {
         'token' : register_create_channel['valid_token'],
         'name' : "channel",
@@ -261,7 +384,49 @@ def test_invalid_message(register_create_channel):
 
     new_channel_id = requests.post(config.url + '/channels/create/v2', json=new_channel_create_input).json()['channel_id']
 
-    messagedict = send_message(register_create_channel, 1)
+    messagedict = send_message(register_create_channel['valid_token'], register_create_channel['valid_channel_id'], 1)
+
+    auth_register_input = {
+        'email': "newperson@gmail.com",
+        'password': "password",
+        'name_first': "First",
+        'name_last': "Last"
+    }
+
+    member = requests.post(config.url + 'auth/register/v2', json=auth_register_input).json()
+
+    channel_join_input = {
+        'token' : member['token'],
+        'channel_id' : new_channel_id
+    }
+
+    requests.post(config.url + '/channel/join/v2', json=channel_join_input)
+
+    message_share_input = {
+        'token': member['token'],
+        'og_message_id': messagedict['message_id_list'][0],
+        'message': "World!",
+        'channel_id': new_channel_id,
+        'dm_id': -1
+    }
+
+    status = requests.post(config.url + 'message/share/v1', json=message_share_input)
+
+    assert status.status_code == InputError.code 
+
+def test_invalid_message(register_create_channel):
+    '''
+    Tests invalid message that's too long
+    '''
+    new_channel_create_input = {
+        'token' : register_create_channel['valid_token'],
+        'name' : "channel",
+        'is_public' : True
+    }
+
+    new_channel_id = requests.post(config.url + '/channels/create/v2', json=new_channel_create_input).json()['channel_id']
+
+    messagedict = send_message(register_create_channel['valid_token'], new_channel_id, 1)
 
     message_share_input = {
         'token': register_create_channel['valid_token'],
@@ -271,11 +436,14 @@ def test_invalid_message(register_create_channel):
         'dm_id': -1
     }
 
-    status = requests.post(config.url + 'message/share/v1', json=message_share_input).json()
+    status = requests.post(config.url + 'message/share/v1', json=message_share_input)
 
     assert status.status_code == InputError.code 
 
 def test_invalid_token(register_create_channel):
+    '''
+    Tests invalid token
+    '''
     new_channel_create_input = {
         'token' : register_create_channel['valid_token'],
         'name' : "channel",
@@ -284,7 +452,7 @@ def test_invalid_token(register_create_channel):
 
     new_channel_id = requests.post(config.url + '/channels/create/v2', json=new_channel_create_input).json()['channel_id']
 
-    messagedict = send_message(register_create_channel, 1)
+    messagedict = send_message(register_create_channel['valid_token'], new_channel_id, 1)
 
     message_share_input = {
         'token': " ",
@@ -294,11 +462,14 @@ def test_invalid_token(register_create_channel):
         'dm_id': -1
     }
 
-    status = requests.post(config.url + 'message/share/v1', json=message_share_input).json()
+    status = requests.post(config.url + 'message/share/v1', json=message_share_input)
 
     assert status.status_code == AccessError.code 
 
-def test_not_member(register_create_channel):
+def test_not_member_shared_channel(register_create_channel):
+    '''
+    Tests not member of channel that message is being shared to
+    '''
     auth_register_input = {
         'email': "newperson@gmail.com",
         'password': "password",
@@ -316,7 +487,7 @@ def test_not_member(register_create_channel):
 
     new_channel_id = requests.post(config.url + '/channels/create/v2', json=new_channel_create_input).json()['channel_id']
 
-    messagedict = send_message(register_create_channel, 1)
+    messagedict = send_message(register_create_channel['valid_token'], new_channel_id, 1)
 
     message_share_input = {
         'token': user_id['token'],
@@ -326,7 +497,7 @@ def test_not_member(register_create_channel):
         'dm_id': -1
     }
 
-    status = requests.post(config.url + 'message/share/v1', json=message_share_input).json()
+    status = requests.post(config.url + 'message/share/v1', json=message_share_input)
 
     assert status.status_code == AccessError.code 
 
